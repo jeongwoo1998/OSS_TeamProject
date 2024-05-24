29a78e91fb0bb8a80f48885c3f7be12cf17abb72
jeongwoo <jeongwoo1998@daum.net>
2024-05-24 18:52:22 +0900

    Update app.py

--- Food recognition/app.py
+++ Food recognition/app.py
@@ -3,15 +3,17 @@ import os
 import json
 import sys
 
-# 모듈 경로 추가
 sys.path.append(os.path.abspath('quantity_est'))
 sys.path.append(os.path.abspath('yolov3'))
 
+# quantity_est, yolov3 음식 인식 모델 함수
 from quantity_est.food_quantity_model import quantity
 from yolov3.food_recognition_model import detect, get_nutrients
 
+# Flask 실행
 app = Flask(__name__)
 
+# '/quantity'에 POST 요청 시 처리
 @app.route('/quantity', methods=['POST'])
 def quantity_route():
     data = request.json
@@ -23,12 +25,13 @@ def quantity_route():
         return jsonify({"error": "image_path is invalid or does not exist"}), 400
 
     try:
-        quantity_value = quantity(image_path)
+        quantity_value = quantity(image_path) # quantity 함수 호출을 통한 음식 양 분석
         response = {"quantity": quantity_value}
         return jsonify(response)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+# '/detect'에 POST 요청 시 처리
 @app.route('/detect', methods=['POST'])
 def detect_route():
     data = request.json
@@ -36,8 +39,8 @@ def detect_route():
     if not image_path:
         return jsonify({"error": "image_path is required"}), 400
 
-    results = detect(image_path)
-    nutrients = get_nutrients(results)
+    results = detect(image_path) # detect 함수 호출을 통한 음식 사진 인식
+    nutrients = get_nutrients(results) # get_nutrients 함수 호출을 통해 인식된 음식의 영양 정보 파악
     response = Response(json.dumps(nutrients, ensure_ascii=False), content_type='application/json; charset=utf-8')
     return response
 
