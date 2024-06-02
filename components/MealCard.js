617045886b694f089e5f6208718e54b6e7762597
FinestKid <sosical@naver.com>
2024-06-02 15:12:37 +0900

    현재 위치 기반 지도 표시 설정 및 검색 기능 임시 구현

--- components/MealCard.js
+++ components/MealCard.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Image, TouchableOpacity } from 'react-native';
 import { Card, Text, Button } from 'react-native-paper';
 import { AppStyles as styles } from '../styles/AppStyles';
+import * as ImagePicker from 'expo-image-picker';
 
 // mealTitle : 식사 제목, date : 식사 날짜, imageUri : 식사 이미지 URI, onImagePick : 이미지 선택 시 호출되는 콜백함수
 const MealCard = ({ mealTitle, date, navigation, imageUri, onImagePick }) => {
+  const [selectedImage, setSelectedImage] = useState(null); // 선택된 이미지 URI 저장, 기본값은 null
   // 해당 식사 정보와 함께 영양 정보 화면으로 이동
   const handleNutritionInfo = () => {
     navigation.navigate('NutritionInfo', { mealTitle, date: date.toISOString(), imageUri });
+  };
+
+  // 이미지 선택 함수
+  const handleImagePick = async () => {
+    // 이미지 선택을 위한 권한 요청, 권한 요청의 결과는 status 변수에 할당
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    // status 값을 확인했을 때 권한이 허용되지 않은 경우('granted'가 아닌 경우) 아래 알림 메시지 표시
+    if (status !== 'granted') {
+      alert('접근 권한이 필요합니다.');
+      return;
+    }
+
+    // 이미지 선택, 선택 결과는 result 변수에 할당
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    // 이미지 선택이 취소되지 않았다면 이미지 정보 전달
+    if (!result.cancelled) {
+      onImagePick(mealTitle, result.uri); // 선택한 이미지의 URI를 전달
+      setSelectedImage(result.uri); // 선택된 이미지의 URI 저장
+    }
   };
 
   return (
@@ -17,12 +44,12 @@ const MealCard = ({ mealTitle, date, navigation, imageUri, onImagePick }) => {
 
       {/* 각 카드 이미지 */}
       <Card.Content style={styles.mealContent}>
-        {/* imageUri가 있을 경우 이미지 표시 */}
-        {imageUri ? (
-          <Image source={{ uri: imageUri }} style={styles.mealImage} />
+        {/* 선택된 이미지가 있을 경우 이미지 표시 */}
+        {selectedImage ? (
+          <Image source={{ uri: selectedImage }} style={styles.mealImage} />
         ) : (
-          // imageUri가 없을 경우 '이미지를 선택하세요' 글자만 떠 있는 빈 이미지 출력, 이미지 클릭 시 이미지 선택하는 콜백함수 호출
-          <TouchableOpacity onPress={() => onImagePick(mealTitle)} style={styles.imagePicker}>
+          // 선택된 이미지가 없을 경우 '이미지를 선택하세요' 글자만 떠 있는 빈 이미지 출력, 이미지 클릭 시 이미지 선택하는 콜백함수 호출
+          <TouchableOpacity onPress={handleImagePick} style={styles.imagePicker}>
             <Text style={[styles.imagePickerText, { textAlign: 'center' }]}>이미지를 선택하세요</Text>
           </TouchableOpacity>
         )}
--- screens/RestaurantRecommendationScreen.js
+++ screens/RestaurantRecommendationScreen.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { View, TouchableOpacity, StyleSheet, SafeAreaView, StatusBar } from 'react-native';
-import { Text } from 'react-native-paper';
+import { View, TouchableOpacity, StyleSheet, SafeAreaView, StatusBar, TextInput } from 'react-native';
+import { Text, Button } from 'react-native-paper';
 import Header from '../components/Header';
 import { AppStyles as styles } from '../styles/AppStyles';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import MenuScreen from '../components/MenuScreen';
+import * as Location from 'expo-location';
+import api from '../utils/api'
 
 
 const RestaurantRecommendationScreen = ({ navigation }) => {
@@ -17,18 +19,51 @@ const RestaurantRecommendationScreen = ({ navigation }) => {
     latitudeDelta: 0.0922,    // 지도의 위도 범위
     longitudeDelta: 0.0421,   // 지도의 경도 범위
   });
+  const [searchKeyword, setSearchKeyword] = useState('');  // 사용자가 입력한 검색어 저장
+  const [searchResults, setSearchResults] = useState([]);  // 검색 결과 저장
 
   const toggleMenu = () => {    // showMenu 상태를 반전시켜 메뉴 화면을 열거나 닫음
     setShowMenu(!showMenu);
   };
 
   useEffect(() => {
+    // 사용자의 현재 위치 정보 가져오기
+    const fetchUserLocation = async () => {
+      try {
+        // 위치 정보 접근 권한 요청
+        const { status } = await Location.requestForegroundPermissionsAsync();
+        // status 값을 확인했을 때 권한이 허용되지 않은 경우('granted'가 아닌 경우) 아래 알림 메시지 표시
+        if (status !== 'granted') {
+          console.error('접근 권한이 필요합니다.');
+          return;
+        }
+
+        // 사용자의 현재 위치 가져오기
+        const location = await Location.getCurrentPositionAsync({});
+        // Marker를 표시하기 위해 현재 위치 저장
+        const { latitude, longitude } = location.coords;
+
+        // 가져온 위치 정보로 지도 영역 업데이트
+        setMapRegion(prevRegion => ({
+          ...prevRegion,
+          latitude,
+          longitude,
+        }));
+      } catch (error) {
+        console.error('Error fetching user location:', error);
+      }
+    };
+
+    fetchUserLocation();
+  }, []);
+
+  useEffect(() => {
     // 서버로부터 추천 음식 데이터 가져오기
     const fetchRecommendedFoods = async () => {
       try {
         // 여기에서 NutritionStatsScreen에서 총 영양소 섭취량 데이터를 받아와야 함
-        
-        const recommendedFoods = ['한식', '중식', '일식'];  // 임의 데이터
+        // const response = await api.get('/api/recommended-foods');  // 서버 연결 시 쓸 코드 
+        const recommendedFoods = ['한식', '중식', '일식']; // 임의 데이터
         setRecommendedFoods(recommendedFoods);  // setRecommendedFoods를 호출하여 받아온 음식 데이터로 recommendedFoods 상태를 업데이트함
       } catch (error) {
         // 서버 요청 실패 혹은 타임아웃 오류 발생 시 에러 메시지 출력
@@ -39,10 +74,40 @@ const RestaurantRecommendationScreen = ({ navigation }) => {
     fetchRecommendedFoods();
   }, []);
 
-  const handleFoodSelect = (food) => {
-    setSelectedFood(food);
-    // 여기에서 선택된 음식에 따라 지도 데이터를 업데이트하는 로직을 구현해야 함
-    // 예를 들어, 카카오맵 API를 사용하여 해당 음식에 대한 근처 식당 데이터를 가져와 지도에 표시하기
+
+  // const handleSearch = async () => {
+  //   // 검색 처리 함수
+  //   try {
+  //     // 검색 키워드를 사용하여 서버에 검색 요청 보내기, 응답 결과는 response에 전달
+  //     const response = await api.get('/api/search', {
+  //       params: {
+  //         keyword: searchKeyword,  // params 객체에 keyword라는 키로 검색어(searchKeyword) 전달
+  //       },
+  //     });
+  //     setSearchResults(response.data.results);  // 서버로부터 받은 응답의 data.results를 searchResults 상태에 저장
+  //   } catch (error) {
+  //     console.error('Error searching:', error);
+  //   }
+  // };
+
+  const handleFoodSelect = async (food) => {
+    // 음식 선택 처리 함수
+    try {
+      // 선택된 음식을 selectedFood 상태에 저장
+      setSelectedFood(food);
+      // 선택된 음식과 현재 위치를 기반으로 서버에 음식점 검색 요청 보내기
+      const response = await api.get('/api/restaurants', {
+        params: {  // params 객체에 food(음식 이름), latitude, longitude(위도, 경도)를 담아 서버에 전달
+          food,
+          latitude: mapRegion.latitude,
+          longitude: mapRegion.longitude,
+        },
+      });
+      // 그럼 서버로부터 id, name, address, latitude, longitude를 전달받아야 함
+      setSearchResults(response.data.results);
+    } catch (error) {
+      console.error('Error searching restaurants:', error);
+    }
   };
 
   return showMenu ? (
@@ -52,7 +117,20 @@ const RestaurantRecommendationScreen = ({ navigation }) => {
     // Header 컴포넌트, 현재 기기의 상태 표시줄의 높이만큼 상단에 패딩을 추가
     <SafeAreaView style={[styles.container, { paddingTop: StatusBar.currentHeight }]}>
       <Header navigation={navigation} toggleMenu={toggleMenu} title="식당 추천" />
-    
+   
+      {/* 검색 기능 */}
+      {/* <View>
+        <TextInput
+          style
+          placeholder="검색어를 입력하세요"  // 입력 필드가 비어 있을 때 표시되는 텍스트
+          value={searchKeyword}
+          onChangeText={setSearchKeyword}
+        />
+        <Button mode="contained" onPress={handleSearch}>  // 버튼을 누르면 검색 처리 함수를 호출함
+          검색
+        </Button>
+      </View> */}
+
       {/* map 함수를 통해 음식 버튼 3개 렌더링 */}
       <View style={styles.foodButtonContainer}>
         {recommendedFoods.map((food) => ( // recommendedFoods 배열에 있는 음식 데이터가 각 버튼의 텍스트 
@@ -80,7 +158,19 @@ const RestaurantRecommendationScreen = ({ navigation }) => {
       </View>
 
       {/* MapView를 이용해 지도 영역 렌더링. mapRegion 상태 변수의 값을 전달해 지도의 위치와 확대 수준 설정 */}
-      <MapView style={styles.mapContainer} region={mapRegion} />
+      <MapView style={styles.mapContainer} region={mapRegion}>
+        {searchResults.map((result) => (
+          <Marker
+            key={result.id}  // 마커에 고유한 key 설정. React가 각 마커를 고유하게 식별하도록 설정
+            coordinate={{
+              latitude: result.latitude,  // 마커가 찍힐 위도
+              longitude: result.longitude,  // 마커가 찍힐 경도
+            }}
+            title={result.name}  // 마커의 이름 (식당 이름)
+            description={result.address}  // 마커의 설명 (식당 설명)
+          />
+        ))}
+      </MapView>
     </SafeAreaView>
   );
 };
