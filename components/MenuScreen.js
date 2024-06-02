491f102e10471f206b6da22fe5faa38bb939ad5f
FinestKid <sosical@naver.com>
2024-06-02 11:36:24 +0900

    메뉴 화면 변경

--- components/MenuScreen.js
+++ components/MenuScreen.js
@@ -9,14 +9,14 @@ const menuItems = [
   { label: '목표 섭취량 설정', screen: 'GoalSetting' },
 ];
 
-const MenuScreen = ({ navigation, closeMenu }) => {
+const MenuScreen = ({ navigation, closeMenu }) => {  // menuItems.map 메서드를 이용해 배열의 각 하목에 대해 TouchableOpacity 컴포넌트 생성
   return (
     <View style={styles.menuContainer}>
       {menuItems.map((item) => (
         <TouchableOpacity
           key={item.screen}
           style={styles.menuItem}
-          onPress={() => {
+          onPress={() => {    // 누르면 메뉴 화면을 닫고 해당 화면으로 네비게이션
             closeMenu();
             navigation.navigate(item.screen);
           }}
