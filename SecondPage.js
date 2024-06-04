00ffb013068583fdcc12205149b5c872a77083ec
MinHyekok <mi2956571@gmail.com>
2024-06-04 23:35:06 +0900

    setting SecondPage.js

---
+++ SecondPage.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { View, Text, Image, StyleSheet, TouchableOpacity, Dimensions, Modal, ScrollView } from 'react-native';
+
+const { width } = Dimensions.get('window');
+
+export default function SecondPage({ navigation }) {
+  const [genderVal, setGenderVal] = useState('');
+  const [weight, setWeight] = useState('60 kg');
+  const [height, setHeight] = useState('176 cm');
+  const [weightModalVisible, setWeightModalVisible] = useState(false);
+  const [heightModalVisible, setHeightModalVisible] = useState(false);
+
+}
